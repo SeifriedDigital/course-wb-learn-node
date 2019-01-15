b64ce534b5cbeb03d16bf139306e8128d0a72744
Mathew Seifried <mathew@seifrieddigital.com>
2019-01-14 22:44:49 -0500

    Completed Modules 3 up to 4.3
    Completed modules from 3 up to 4.3. Next module is "Querying our Database for Stores".

--- app/controllers/storeController.js
+++ app/controllers/storeController.js
@@ -1,8 +1,20 @@
-exports.myMiddleware = (req, res, next) => {
-    req.name = 'Mat';
-    next();
-}
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
-    res.render('index');
+  
+  res.render('index');
+};  
+
+exports.addStore = (req, res) => {
+    res.render('editStore', {
+        title: 'Add Store'
+    });
+};  
+
+exports.createStore = async (req, res) => {
+    const store = await (new Store(req.body)).save();
+    await store.save();
+    req.flash('success', `Successfully created ${store.name}. care to leave a review?`);
+    res.redirect(`/store/${store.slug}`);
 };
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 // Require storeController to control store requests in seperate file... organization matters!!!
-
 const storeController = require('../controllers/storeController');
+const { catchErrors } = require('../handlers/errorHandlers');
 
-router.get('/', storeController.myMiddleware, storeController.homePage);
+router.get('/', storeController.homePage);
+router.get('/add', storeController.addStore);
+router.post('/add', catchErrors(storeController.createStore));
 
 module.exports = router;
