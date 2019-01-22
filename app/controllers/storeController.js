e143d6d731def3a018c6fdb738f8524756f7ce77
Mathew Seifried <mathew@seifrieddigital.com>
2019-01-22 01:04:45 -0500

    Compled Module 5
    Completed module 5. Created a work flow for managing store data CRU of total CRUD workflow.

--- app/controllers/storeController.js
+++ app/controllers/storeController.js
@@ -18,3 +18,39 @@ exports.createStore = async (req, res) => {
     req.flash('success', `Successfully created ${store.name}. care to leave a review?`);
     res.redirect(`/store/${store.slug}`);
 };
+
+exports.getStores = async (req, res) => {
+  // Query database for a list of the stores
+  const stores = await Store.find();
+  console.log(stores);
+
+  res.render('stores', {
+    title: 'Stores',
+    stores
+  });
+};
+
+exports.editStore = async (req, res) => {
+  // Find the store given the ID
+  const store = await Store.findOne({ _id: req.params.id })
+  // Confirm they are the owner of the store
+  // TODO
+
+  // Render out the edit form so the user can update their store
+  res.render('editStore', {
+    title: `Edit ${store.name}`,
+    store
+  });
+};
+
+exports.updateStore = async (req, res) => {
+  // Find and update the store
+  const store = await Store.findOneAndUpdate({_id: req.params.id}, req.body, {
+    new: true, // return the new store insted of the old one
+    runValidators: true
+  }).exec();
+  req.flash('success', `Successfully updated ${store.name}. <a href="/stores/${store.slug}">View Store -></a>`);
+  res.redirect(`/stores/${store._id}/edit`);
+
+  // Redirect them to the store and tell them it worked
+}
